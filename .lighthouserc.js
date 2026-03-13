c6f8e6864d9222961ec61384eb2e06b6b5e8b3f8
Saavedra Ramon <102835876+Papyrus-Saa@users.noreply.github.com>
2026-03-13 14:50:21 +0100

    fix(lighthouse): update server command and timeout in Lighthouse CI config

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,8 +2,8 @@ module.exports = {
   ci: {
     collect: {
       url: ['http://localhost:3001'],
-      startServerCommand: 'npm run start',
-      startServerReadyTimeout: 30000
+      startServerCommand: 'npm run start -- -p 3001',
+      startServerReadyTimeout: 60000
     },
     upload: {
       target: 'temporary-public-storage'
